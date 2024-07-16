2aec64891d87845a26468e478ed1b3bf1fb5f253
sneha4080 <163323422+sneha4080@users.noreply.github.com>
2024-07-16 23:27:56 +0530

    Update middleware.js
    add the authenticate logIn middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,25 @@
+module.exports.isLoggedIn = (req, res, next) => {
+      // Check if user is authenticated (assuming Passport)
+      if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be logged in to create listing!');
+        return res.redirect('/login');
+      }
+    
+      // If user is authenticated, proceed to the next middleware
+      next();
+    };
+
+// module.exports.isLoggedIn = (req, res, next) => {
+//       if (!req.isAuthenticated()) {
+//         req.flash('error', 'You must be logged in to create listing!');
+//         return res.redirect('/login');
+//       }
+//       next();
+//     };
+    
+
+
+
 const validateReview = ((req, res, next) => {
     let { error } = reviewSchema.validate(req.body);
     if (error) {
@@ -7,3 +29,13 @@ const validateReview = ((req, res, next) => {
           next();
     }
 });
+
+const validateListing = ((req, res, next) => {
+      let { error } = reviewSchema.validate(req.body);
+      if (error) {
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+      } else {
+            next();
+      }
+  });
