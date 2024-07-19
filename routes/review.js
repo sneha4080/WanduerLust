e67ddc6c69c0c385edd4ca4750c2ef4d7658c85d
sneha4080 <163323422+sneha4080@users.noreply.github.com>
2024-07-19 13:39:23 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js") //use create route 
-const ExpressError = require("../utils/ExpressError.js");
 const {reviewSchema} = require("../schema.js");
 const Listing = require("../models/Listing.js");//listing file ne require kari
-const Review = require("../models/review.js")
+// const { validateReview } = require("../middleware.js")
+
+
 
 
 const validateReview = ((req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-          let errMsg = error.details.map((el) => el.message).join(",");
-          throw new ExpressError(400, errMsg);
-    } else {
-          next();
-    }
-});
-
+      let { error } = reviewSchema.validate(req.body);
+      if (error) {
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+      } else {
+            next();
+      }
+  });
 
 
 //Reviwes post route
