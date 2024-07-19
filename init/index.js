f72932076667229abc88d5f083d38dff8f8a13f7
sneha4080 <163323422+sneha4080@users.noreply.github.com>
2024-07-19 13:40:53 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -5,21 +5,26 @@ const listing = require("../models/Listing.js");
 const MONGO_URl = "mongodb://127.0.0.1:27017/Wanderlust";
 
 main()
- .then(()=>{
-   console.log("connected to DB");
-}).catch((err)=>{
-      console.log(err);
-})
+      .then(() => {
+            console.log("connected to DB");
+      }).catch((err) => {
+            console.log(err);
+      })
 
 
 
-async  function main() {
+async function main() {
       await mongoose.connect(MONGO_URl);
 }
 
-const initDB = async ()=>{
-    await listing.deleteMany({}) //db ne empty karvah
-    await listing.insertMany(initData.data); //initData aek obj 
-    console.log("data was initialized");
+
+const initDB = async () => {
+      await listing.deleteMany({}) //db ne empty karvah
+      initData.data = initData.data.map((obj) => ({
+            ...obj, owner : "669947484756149521cd275e"
+      }))
+      await listing.insertMany(initData.data); //initData aek obj 
+      console.log("data was initialized");
 }
 initDB();
+
